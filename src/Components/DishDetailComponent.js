1bf48364246e056eb6eda4ff30150507ca8c2c3b
Ajit Walwadkar <ajw.1490@gmail.com>
2021-04-10 19:36:33 +0530

    Assignment 1 - Dish details component

---
+++ src/Components/DishDetailComponent.js
@@ -0,0 +1,60 @@
+import { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle><h4>{dish.name}</h4></CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    renderComments(dish) {
+        if (dish != null && dish.comments != null)
+            return(
+                <Card>
+                    <CardBody>
+                      <CardTitle ><h4>Comments</h4></CardTitle>
+                        <CardText>
+                            <ul className="list-unstyled">
+                                {dish.comments.map((c) => <li>{c.comment}<br/>--{c.author}, {c.date}<br/><br /></li>)}
+                            </ul>
+                        </CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.selectedDish)}
+                </div>
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;

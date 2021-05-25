8a738abb2410260fa260d9029f9b44e2ef6c2fc0
Ajit Walwadkar <ajw.1490@gmail.com>
2021-05-25 18:30:27 +0530

    Fetch from server

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Component } from "react";
 import { Button, Modal, ModalHeader, ModalBody, Col, Row, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -13,7 +14,7 @@ function RenderDish(dish) {
     if (dish != null)
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle><h4>{dish.name}</h4></CardTitle>
                     <CardText>{dish.description}</CardText>
